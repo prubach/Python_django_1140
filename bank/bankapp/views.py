c065f356eb5b2057a9b51dcfa8da24b53cf0c28e
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-01-14 13:12:07 +0100

    Hello message using a html template

--- bank/bankapp/views.py
+++ bank/bankapp/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse('Welcome to My Bank!!!')
 
+
 def hello(request):
     msg = 'Hello Text inside a template'
-    return render(request, 'test.html', { 'hello_text' : msg})
+    return render(request, 'test.html', {'hello_text': msg})
