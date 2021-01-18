746cac95aefcc52454be9b68f57491a5b42a7588
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-01-18 14:39:50 +0100

    Rest framework added

--- bank/bankapp/urls.py
+++ bank/bankapp/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'customers', views.CustomerViewSet)
+router.register(r'accounts', views.AccountViewSet)
 
 urlpatterns = [
     path('', views.index, name='Index'),
     path('hello', views.hello, name='Hello'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- bank/bankapp/views.py
+++ bank/bankapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
 
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer, CustomerSerializer, AccountSerializer
+from .models import Customer, Account
 # Create your views here.
 
 
@@ -11,3 +16,38 @@ def index(request):
 def hello(request):
     msg = 'Hello Text inside a template'
     return render(request, 'test.html', {'hello_text': msg})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class AccountViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
