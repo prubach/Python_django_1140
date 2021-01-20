70e7230761631a919ad085892cf52152da94284a
Paweł Rubach <pawel.rubach@gmail.com>
2021-01-20 19:44:42 +0100

    Added docker files

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run --rm -it -p 8020:8020 -e DJANGO_SUPERUSER_USERNAME=admin -e DJANGO_SUPERUSER_PASSWORD=sekret1 -e DJANGO_SUPERUSER_EMAIL=admin@example.com django-bank
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd bank
+python3 manage.py runserver 0.0.0.0:8020
+
